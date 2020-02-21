6fe55ba52d20a764cff4e517be3fdf57c902455b
Lope Ariyo <lopeariyo@gmail.com>
2020-02-21 11:00:38 +0000

    circular slider now rendering

--- src/pages/Cycle.js
+++ src/pages/Cycle.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import CircularSlider from "react-circular-slider-svg";
 
 //TODO
 //+ get the cycle start date
@@ -9,90 +10,35 @@ import React from "react";
 //+ for each element in the cycle array render a div to represent the cycle days
 //+ each circle should display the cycle day number
 //+ each circle also needs a degree so that it appears as a border around the main circle
+const Cycle = () => {
+    const [value1, setValue1] = useState(1);
 
-class Cycle extends React.Component {
-    state = {
-        cycleStartDate: "02/01/2020",
-        cycleEndDate: "03/02/2020",
-        cycle: []
-    };
-
-    // componentDidMount() {
-    //     if (this.props.user === null) {
-    //         this.props.history.push("/");
-    //     }
-    // }
-
-    //Reminder
-    //& cycleStartDate and cycleEndDate will come from props
-
-    getCycleLength = () => {
-        let startDate = new Date(this.state.cycleStartDate);
-        let endDate = new Date(this.state.cycleEndDate);
-
-        let cycleLength =
-            (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24);
-
-        return cycleLength;
-    };
-
-    createCycle = () => {
-        let cycleLength = this.getCycleLength();
-        let cycle = [];
-
-        for (let i = 0; i <= cycleLength; i++) {
-            cycle.push((i + 1).toString());
-        }
-
-        this.setState({ cycle: cycle });
-    };
-
-    componentDidMount() {
-        this.createCycle();
-    }
-
-    // (() => {
-    //     console.log(
-    //         `rotate(${index * cycleDaysDegree}deg)`
-    //     );
-    //     return {
-    //         "transform-origin": `rotate(${index *
-    //             cycleDaysDegree}deg)`,
-    //         color: "pink"
-    //     };
-    // })()
-
-    render() {
-        const cycleDaysDegree =
-            (360 - this.getCycleLength() / 2) / this.getCycleLength();
-
-        return (
-            <div className="Page">
-                <h3>Your Cycle</h3>
-                <div>
-                    {/* <div className="InfoCircle"></div> */}
-                    {/* "translate(210px, -50vh)" */}
-                </div>
-                <div className="CycleCircle">
-                    {this.state.cycle.map((day, index) => (
-                        <div
-                            className="CycleDay"
-                            key={index}
-                            style={{
-                                position: "absolute",
-                                transformOrigin: "100% 27vh",
-                                transform: `rotate(${index *
-                                    cycleDaysDegree}deg)`
-                            }}
-                        >
-                            {" "}
-                            Day {day}{" "}
-                        </div>
-                    ))}
-                </div>
+    return (
+        <div className="Page">
+            <h3>Your Cycle</h3>
+            <div>
+                <CircularSlider
+                    size={300}
+                    startAngle={0}
+                    endAngle={360}
+                    minValue={0}
+                    maxValue={28} //cycle length - 1
+                    angleType={{
+                        direction: "cw",
+                        axis: "+y"
+                    }}
+                    handle1={{
+                        value: value1,
+                        onChange: v => setValue1(v)
+                    }}
+                    arcColor="#690"
+                />
+                {/* <div className="InfoCircle"></div> */}
+                {/* "translate(210px, -50vh)" */}
             </div>
-        );
-    }
-}
+            <div className="CycleCircle"></div>
+        </div>
+    );
+};
 
 export default Cycle;
